5cce8a5bb1186f3f3aed959918188f57f4a317a3
Augusto Ojeda <okarinex@gmail.com>
2022-10-24 12:09:48 -0300

    external connection

--- db/dbConnection.ts
+++ db/dbConnection.ts
@@ -7,8 +7,8 @@ const { DB_NAME,
         DB_PASSWORD,
         DB_HOST
     } = process.env
-
-
+    
+    
 export const sequelize = new Sequelize( `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?ssl=true`, {
     logging: false,
     native: false,
