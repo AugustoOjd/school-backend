36cc2c913cdf55b5fd08fec85427f5ae223ff23c
Augusto Ojeda <okarinex@gmail.com>
2022-10-24 13:57:22 -0300

    cors origin

--- models/server.ts
+++ models/server.ts
@@ -72,7 +72,7 @@ class Server {
 
         this.app.use( cors(
             {
-                origin: 'https://u-culture-augustoojd.vercel.app/',
+                origin: '*',
                 methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
                 credentials: true,
               }
